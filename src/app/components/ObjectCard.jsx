d406a863af1501be952c05738c695d96dee90818
jorgerf05 <jorgefco_rf@hotmail.com>
2023-11-26 03:26:41 -0800

    Ready for fetching API

--- src/app/components/ObjectCard.jsx
+++ src/app/components/ObjectCard.jsx
@@ -17,22 +17,26 @@ const ObjectCard = ({ object }) => {
 
   return (
     <div
-      className="flex flex-col h-max p-8 bg-white rounded-2xl shadow-xl hover:bg-blue-200 duration-500"
+      className="flex flex-col h-max w-max p-8 bg-white rounded-2xl shadow-xl hover:bg-blue-200 duration-500"
       onClick={() => setexpanded(!expanded)}
     >
       <div className="flex space-x-5 justify-evenly text-xl">
         <div className="font-bold text-black">{object_name}</div>
         <div className=" text-black">{claimer_fullname}</div>
         <div className=" text-black">{report_date}</div>
-        <div className="text-red-500">{status}</div>
+        {status === 'Perdido' ? <div className="text-red-500">{status}</div> : <div className="text-green-500">{status}</div>}
       </div>
       {expanded ? (
-        <div className="bg-white transition-transform rounded-2xl p-4 text-xl text-black">
+        <div className="flex flex-col bg-white transition-transform rounded-2xl p-4 text-xl text-black">
           <div>Carrera: {claimer_career}</div>
           <div>Semestre: {claimer_semester}</div>
           <div>Tipo de personal: {claimer_type}</div>
           <div>Visto por ultima vez: {last_location}</div>
           <div>Contacto: {claimer_contact}</div>
+          <div className="flex mt-3 self-center justify-self-center space-x-3 text-xl font-bold">
+            <div className="bg-green-300 p-2 rounded-2xl">Entregar</div>
+            <div className="bg-red-400 p-2 rounded-2xl">Eliminar</div>
+          </div>
         </div>
       ) : null}
     </div>
--- src/app/object_requests/page.js
+++ src/app/object_requests/page.js
@@ -1,9 +1,41 @@
 import { Table } from "@mui/joy";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CustomTable from "../components/Table";
 import ObjectCard from "../components/ObjectCard";
 
+function filterObjectsBySubstring(objects, searchString) {
+  // Filter the array of objects
+  return objects.filter((obj) => {
+    // Check each property of the object
+    for (let key in obj) {
+      // If the property contains the search string, return true
+      if (
+        obj.hasOwnProperty(key) &&
+        typeof obj[key] === "string" &&
+        obj[key].includes(searchString)
+      ) {
+        return true;
+      }
+    }
+    // If no property contains the search string, return false
+    return false;
+  });
+}
+
+// Example usage
+const objects = [
+  { name: "John", age: 25, city: "New York" },
+  { name: "Jane", age: 30, city: "Los Angeles" },
+  { name: "Bob", age: 22, city: "Chicago" },
+];
+
+const filteredObjects = filterObjectsBySubstring(objects, "o");
+console.log(filteredObjects);
+
 export default function Home() {
+  const [data, setData] = useState([{}]);
+  const [filter, setfilter] = useState("");
+
   function createData(
     report_date,
     claimer_fullname,
@@ -71,15 +103,30 @@ export default function Home() {
         "Laboratorio de Redes",
         "6461891906",
         "Perdido"
+      ),
+      createData(
+        "12 de Junio de 2020",
+        "Jorge Rodriguez Flores",
+        "Sistemas Computacionales",
+        "8",
+        "Alumno",
+        "Celular Xiaomi Poco",
+        "Laboratorio de Redes",
+        "6461891906",
+        "Encontrado"
       )
     );
   }
+
+  const rows2 = filterObjectsBySubstring(rows, "Encontrado");
   return (
-    <main className="bg-gray-200 flex flex-col w-full space-y-5 p-5">
-      <div className="flex text-black text-3xl font-bold mb-5 ml-5">Objetos registrados</div>
-        {rows.map((obj) => (
-          <ObjectCard key={obj.report_date} object={obj} />
-        ))}
+    <main className="bg-gray-200 flex flex-col w-full space-y-5 p-5 content-center items-center">
+      <div className="flex text-black text-3xl font-bold mb-5 ml-5">
+        Objetos registrados
+      </div>
+      {rows2.map((obj) => (
+        <ObjectCard key={obj.report_date} object={obj} />
+      ))}
     </main>
   );
 }
